11b6ce00868a1a9af84479a04e1cff9d906b7a67
DSTLC-V.S.I.N <dstlc.vsin@gmail.com>
2017-10-28 10:39:16 +0900

    Add 共通ライブラリ

---
+++ CommonLibrary/DMessenger/Message.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace DMessenger
+{
+    public sealed class Message
+    {
+        public bool IsDeleted { get; private set; }
+        public MessageThread Thread { get; internal set; }
+        public Guid MessageId { get; }
+        public MessagePriority Priority { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public XElement Value { get; set; }
+
+        public void Delete()
+        {
+            IsDeleted = true;
+            Value = null;
+            UpdateTime = DateTime.Now;
+        }
+
+        public XElement ToXml()
+        {
+            var xe = new XElement("message");
+            xe.SetAttributeValue("id", MessageId.ToString("N")); // メッセージID
+            xe.SetAttributeValue("update", UpdateTime); // 更新日時
+            if (Thread != null)
+                xe.SetAttributeValue("thread", Thread.ThreadId);
+
+            if (IsDeleted)
+            {
+                xe.SetAttributeValue("deleted", "true");
+            }
+            else
+            {
+                xe.Add(new XElement(Value)); // 本文
+                if (Priority != MessagePriority.Normal)
+                    xe.SetAttributeValue("priority", Priority.ToString()); // 緊急度
+            }
+
+            return xe;
+        }
+    }
+
+    public enum MessagePriority
+    {
+        Talk = -1,
+        Normal = 0,
+        Emergency = 1
+    }
+}
---
+++ CommonLibrary/DMessenger/MessageThread.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMessenger
+{
+    public sealed class MessageThread : IReadOnlyList<Message>
+    {
+        private readonly List<Message> messages = new List<Message>();
+
+        public Message this[int index] => messages[index];
+
+        public int Count => messages.Count;
+        public Guid ThreadId { get; }
+
+        /// <summary>
+        /// 作成または受信したメッセージをスレッドに追加します。
+        /// </summary>
+        /// <param name="value"></param>
+        public bool AddOrUpdate(Message value)
+        {
+            lock (messages)
+            {
+                // 同一IDのメッセージがないか確認する
+                foreach (var item in messages)
+                {
+                    if (item.MessageId == value.MessageId)
+                    {
+                        if (item.UpdateTime < value.UpdateTime)
+                        {
+                            messages.Remove(item); // 旧メッセージを削除
+                            break;
+                        }
+                        else
+                            return false;
+                    }
+                }
+
+                // 確認できたので、スレッドに追加
+                for (int i = messages.Count - 1; i >= 0; i--)
+                {
+                    if (messages[i].UpdateTime < value.UpdateTime)
+                    {
+                        messages.Insert(i + 1, value);
+                        return true;
+                    }
+                }
+                messages.Add(value); // 一番新しいメッセージの場合
+                return true;
+            }
+        }
+
+        public IEnumerator<Message> GetEnumerator()
+        {
+            lock (messages)
+            {
+                return (IEnumerator<Message>)messages.ToArray().GetEnumerator();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
