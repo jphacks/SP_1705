d7199f59c255afa74dd119502ab238fd71799d63
DSTLC-V.S.I.N <dstlc.vsin@gmail.com>
2017-10-28 16:39:13 +0900

    Add MessageItem

---
+++ WinMessenger/WinMessenger/DB/MessageItem.cs
@@ -0,0 +1,19 @@
+﻿using DMessenger;
+using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinMessenger.DB
+{
+    public class MessageItem
+    {
+        [PrimaryKey]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public MessagePriority Priority { get; set; }
+        public DateTime UpdateTime { get; set; } = DateTime.UtcNow;
+        public string Value { get; set; }
+    }
+}
